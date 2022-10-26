534bb1ca9b8b05a1a59b05d19bce56396abbff15
awadh <awadh.hassan@student.moringaschool.com>
2022-10-27 00:34:58 +0300

    donr

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+
+   def badge_maker
+     "Hello, my name is #{name}." 
+    end
+
+    def batch_badge_creator (names)
+        names.map do |name|
+            "Hello, my name is #{name}."
+        end
+    end
+
+    def assign_rooms speakers
+       speakers.each.with_index(1).map{|speaker, index| "Hello, #{speaker}! You'll be assigned to room #{index}!"}
+    end
+    
+    def printer speakers
+        batch_badge_creator(speakers).each{|speaker|puts speaker}
+        assign_rooms(speakers).each{|speaker|puts speaker}
+    end
